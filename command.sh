eb3aaccfc50cbdab3a4349ea61842fca7ac779b8
ardittot <atrianggada@hadoop-explore.c.virtual-cycling-188314.internal>
2018-01-04 03:14:20 +0000

    first commit

---
+++ command.sh
@@ -0,0 +1,12 @@
+# Install python & pip
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get python-pip python3-pip
+
+# Install java
+sudo apt-get install openjdk-8-jdk openjdk-8-source
+sudo ln -s /usr/lib/jvm/java-1.8.0-openjdk-amd64 /opt/jdk
+echo '' >> ~/.bashrc
+echo 'export JAVA_HOME="/opt/jdk"' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc

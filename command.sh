e666ebbdfbfd98b787232245b62866190b5f12c7
Arditto Trianggada <atrianggada@gmail.com>
2018-01-04 10:16:45 +0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -10,3 +10,14 @@ echo '' >> ~/.bashrc
 echo 'export JAVA_HOME="/opt/jdk"' >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
+
+# Setup hadoop
+make download
+make configure
+make start_hadoop
+make start_spark
+#Hive configuration requires Hadoop/HDFS running
+make configure_hive
+make start_hive_server
+#In another terminal start beeline client
+make start_hive_beeline_client
